4fe80044d6350a3a28610dd58d0ae914ae245168
MohaHOmar <omhd005@gmail.com>
2024-12-07 21:08:55 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -34,7 +34,7 @@ for CliCommPath in $(echo "$tempPath" | tr ',' ' '); do
     echo -e "CLI Output: \n$CLI"
     CLIFULL+="$CLI,"
 done
-if [ $counter -gt 1 ];then 
+if [ $counter -gt 1 ];then
     echo -e "\n\nEach CLI command provides a subset of the values in the gNMI output,
     allowing complete coverage for a thorough comparison."
 fi
@@ -58,21 +58,21 @@ if [ $? -eq 0 ]; then
 fi
 
 newGnmiNorm=$(echo "$newGnmi" | tr -s [A-Z] [a-z] | tr -d "_\n")
-CLIFULLNorm=$(echo "$CLIFULL" | tr -s [A-Z] [a-z] | tr -d "_\n") 
+CLIFULLNorm=$(echo "$CLIFULL" | tr -s [A-Z] [a-z] | tr -d "_\n")
 
 if [ $newGnmiNorm == $CLIFULLNorm ]; then
     echo -e "\n\nMatch After Normalization."
     exit 0
 fi
-##bytes G KB=2^10 = 1024 MB GB M K 
- 
+##bytes G KB=2^10 = 1024 MB GB M K
+
 
 file="comp.txt"
 first_line=$(sed -n '1p' "$file")
 
 if [[ "$first_line" =~ [0-9]+c[0-9]+ ]]; then
     key=$(sed -n '2p' "$file" | cut -d':' -f1 | sed 's/< //')
-    value_gnmi=$(sed -n '2p' "$file" | cut -d':' -f2) 
+    value_gnmi=$(sed -n '2p' "$file" | cut -d':' -f2)
     value_cli=$(sed -n '4p' "$file" | cut -d':' -f2)
     if [[ $value_cli == *"G"* ]];then
         echo -e "\n\nConverting to Match......"
@@ -80,17 +80,32 @@ if [[ "$first_line" =~ [0-9]+c[0-9]+ ]]; then
         value_gnmi=$(echo "$value_cli" | tr -d " ><G")
         value_cli=$(( value_cli * 1000000000 ))
         value_gnmi=$(( value_gnmi * 1000000 ))
-        key="Values" 
+    fi
+    if [[ $value_cli == *"M"* ]];then
+        echo -e "\n\nConverting to Match......"
+        value_cli=$(echo "$value_cli" | tr -d " ><M")
+        value_gnmi=$(echo "$value_cli" | tr -d " ><M")
+        value_cli=$(( value_cli * 1000000 ))
+        value_gnmi=$(( value_gnmi * 1000000 ))
+    fi
+    if [[ $value_cli == *"K"* ]];then
+        echo -e "\n\nConverting to Match......"
+        value_cli=$(echo "$value_cli" | tr -d " ><K")
+        value_gnmi=$(echo "$value_cli" | tr -d " ><K")
+        value_cli=$(( value_cli * 1000 ))
+        value_gnmi=$(( value_gnmi * 1000000 ))
+    fi
+
+        key="Values"
         if [ $value_cli == $value_gnmi ];then
-            echo -e "\n\n Successfully Matched after Conversion" 
+            echo -e "\n\n Successfully Matched after Conversion"
             exit 0
         fi
-    
+
         value_cli=$value_cli" bps"
         value_gnmi=$value_gnmi" bps"
     fi
     #if integer and CLIPathAll contains speed than integer * M
-    
 
     echo -e "\n\n$key differs, showing \"$value_gnmi\" in gNMI and \"$value_cli\" in CLI output."
     exit 0
@@ -101,4 +116,4 @@ if [[ $first_line =~ ^[0-9]+d[0-9]+$ ]]; then
     key=$(sed -n '2p' "$file" | cut -d':' -f1 | sed 's/< //')
     echo -e "\n\n$key is present in the gNMI output but missing in the CLI output."
     exit 0
-fi 
+fi
