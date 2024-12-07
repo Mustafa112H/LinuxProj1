ba2056c9b19fb5c2488660492c4aa67fbab010b2
Mustafa112H <hebamustafa1112@gmail.com>
2024-12-07 18:39:43 +0200

    here

--- GOut.sh
+++ GOut.sh
@@ -1,1 +1,1 @@
-{ "cpu_usage": 75, "user_usage": 45, "system_usage": 20, "idle_percentage": 25 }
+{ "admin_status": "up", "oper_status": "up", "mac_address": "00:1C:42:2B:60:5A", "mtu": 1500, "speed": 1000 }
